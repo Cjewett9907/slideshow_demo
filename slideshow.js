edacfdd655a5086d405ae839495cc541ecddd809
Cjewett_work <Craig9907@gmail.com>
2019-06-15 19:07:00 -0700

    update with more css and optional blinders for the transition

--- slideshow.js
+++ slideshow.js
@@ -11,7 +11,6 @@ function slideShow(){
 
     setInterval(function(){  
         images[i].style.display = "none";
-
         if (i === images.length - 1){
             i = 0
         } else {
@@ -22,9 +21,10 @@ function slideShow(){
     }, 1000);
 }
 
-// This could be accomplished by CSS animations as well
-// for demo purposes this is  done in css
 
+
+// This could be accomplished by CSS animations as well
+// for demo purposes this is done in VanillaDOM Javascript
 function translation(){
     let images2 = document.getElementsByClassName("slideshow-div-img2")
     let i = 0
@@ -48,12 +48,47 @@ function translation(){
         pos += 150
         
     }, 1000);
+}
 
-
+function fader(){
+    let images3 = document.getElementsByClassName("slideshow-div-img3")
+    let i = 0
+    let fadeVar = 9999
+    let fadeSwitch = -1
+    images3[i].style.opacity = "0." + fadeVar ;
+    console.log(fadeVar)
+    
+        setInterval(function(){
+            if (fadeVar < 1000 ) {  
+                fadeVar = 1001 
+                fadeSwitch *= -1 
+                images3[i].style.display = "none"
+                    if (i === images3.length - 1){       
+                        i = 0     
+                    } else {    
+                        i += 1
+                    }
+                images3[i].style.opacity = "0." + fadeVar
+                
+                images3[i].style.display = "block"
+            }
+            if (fadeVar > 10000){
+                images3[i].style.opacity = "1.0"
+                fadeSwitch *= -1
+            } else {
+                images3[i].style.opacity = "0." + fadeVar
+            }
+            
+            console.log("opacity is", "0." + fadeVar)
+            fadeVar += (500 * fadeSwitch) 
+             
+        }, 100);
+    
 }
 
 
 slideShow();
 translation();
+fader();
 
 }
